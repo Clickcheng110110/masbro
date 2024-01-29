568dd1f47160a9e15584a60f087d8b341f480df4
Clickcheng110110 <cl15213298081@gmail.com>
2024-01-29 20:08:22 +0800

    feat: update

--- src/components/Home/BigInt/index.tsx
+++ src/components/Home/BigInt/index.tsx
@@ -1,5 +1,12 @@
-import { Flex, Image, Text, ImageProps, Box } from "@chakra-ui/react";
-import React from "react";
+import {
+  Flex,
+  Image,
+  Text,
+  ImageProps,
+  Box,
+  MergeWithAs,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
 
 import bigInt from "@/assets/images/begInt.png";
 import intIcon1 from "@/assets/images/int-icon-1.png";
@@ -14,15 +21,22 @@ import zkfair from "@/assets/images/zkfair.png";
 import Starknet from "@/assets/images/Starknet.png";
 import linear from "@/assets/images/linear.png";
 import mint from "@/assets/images/mint.png";
+import { motion } from "framer-motion";
+import { buttonHover } from "@/theme/utils/style";
 
-interface BlockChainProps extends ImageProps {
+interface BlockChainProps {
   icon: string;
+  [key: string]: any;
 }
+
+const MotionImage = motion(Image);
 const BlockChain = ({ icon, ...otherProps }: BlockChainProps) => {
-  return <Image w="40px" h="40px" src={icon} {...otherProps} />;
+  return <MotionImage w="40px" h="40px" src={icon} {...otherProps} />;
 };
 
 function Index() {
+  const [isShowAnimation, setIsShowAnimation] = useState(false);
+
   return (
     <>
       <Flex
@@ -50,61 +64,213 @@ function Index() {
             height="88px"
             src={intIcon3}
           />
-          <Flex
-            marginTop="-20px"
-            marginLeft="90px"
-            position="relative"
-            w="300px"
-          >
-            <BlockChain position="absolute" top="80px" left="0" icon={zk} />
-            <BlockChain
-              position="absolute"
-              top="0px"
-              left="40px"
-              icon={Starknet}
-            />
-            <BlockChain
-              position="absolute"
-              top="40px"
-              left="120px"
-              icon={Manta}
-            />
-            <BlockChain
-              position="absolute"
-              top="110px"
-              left="160px"
-              icon={linear}
-            />
-            <BlockChain
-              position="absolute"
-              top="15px"
-              left="200px"
-              icon={zkfair}
-            />
-            <BlockChain
-              position="absolute"
-              top="130px"
-              left="260px"
-              icon={blast}
-            />
-            <BlockChain
-              position="absolute"
-              top="70px"
-              left="310px"
-              icon={Scroll}
-            />
-          </Flex>
+          {isShowAnimation ? (
+            <Flex
+              marginTop="-20px"
+              marginLeft="90px"
+              position="relative"
+              w="300px"
+            >
+              <MotionImage
+                zIndex={11}
+                position="absolute"
+                w="141px"
+                h="111px"
+                top="150px"
+                initial={{
+                  left: 150,
+                }}
+                animate={{
+                  left: [0, 120, 40, 160, 200, 310, 260],
+                }}
+                transition={{
+                  duration: 6,
+                  delay: 0,
+                }}
+                src={intBegger}
+              />
+              <BlockChain
+                position="absolute"
+                top="80px"
+                left="0"
+                initial={{
+                  top: 0,
+                }}
+                icon={zk}
+                animate={{
+                  top: 200,
+                }}
+                transition={{
+                  duration: 1,
+                  delay: 0,
+                }}
+              />
+
+              <BlockChain
+                position="absolute"
+                top="0px"
+                left="40px"
+                initial={{
+                  top: 0,
+                }}
+                animate={{
+                  top: 200,
+                }}
+                transition={{
+                  duration: 1,
+                  delay: 2,
+                }}
+                icon={Starknet}
+              />
+              <BlockChain
+                position="absolute"
+                top="40px"
+                left="120px"
+                initial={{
+                  top: 0,
+                }}
+                animate={{
+                  top: 200,
+                }}
+                transition={{
+                  duration: 1,
+                  delay: 1,
+                }}
+                icon={Manta}
+              />
+              <BlockChain
+                position="absolute"
+                top="110px"
+                left="160px"
+                initial={{
+                  top: 0,
+                }}
+                animate={{
+                  top: 200,
+                }}
+                transition={{
+                  duration: 1,
+                  delay: 3,
+                }}
+                icon={linear}
+              />
+              <BlockChain
+                position="absolute"
+                top="15px"
+                left="200px"
+                initial={{
+                  top: 0,
+                }}
+                animate={{
+                  top: 200,
+                }}
+                transition={{
+                  duration: 1,
+                  delay: 4,
+                }}
+                icon={zkfair}
+              />
+              <BlockChain
+                position="absolute"
+                top="130px"
+                left="260px"
+                initial={{
+                  top: 0,
+                }}
+                animate={{
+                  top: 200,
+                }}
+                transition={{
+                  duration: 1,
+                  delay: 6,
+                }}
+                icon={blast}
+              />
+              <BlockChain
+                position="absolute"
+                top="70px"
+                left="310px"
+                initial={{
+                  top: 0,
+                }}
+                animate={{
+                  top: 200,
+                }}
+                transition={{
+                  duration: 1,
+                  delay: 5,
+                }}
+                icon={Scroll}
+              />
+            </Flex>
+          ) : (
+            <Flex
+              marginTop="-20px"
+              marginLeft="90px"
+              position="relative"
+              w="300px"
+            >
+              <BlockChain position="absolute" top="80px" left="0" icon={zk} />
+              <BlockChain
+                position="absolute"
+                top="0px"
+                left="40px"
+                icon={Starknet}
+              />
+              <BlockChain
+                position="absolute"
+                top="40px"
+                left="120px"
+                icon={Manta}
+              />
+              <BlockChain
+                position="absolute"
+                top="110px"
+                left="160px"
+                icon={linear}
+              />
+              <BlockChain
+                position="absolute"
+                top="15px"
+                left="200px"
+                icon={zkfair}
+              />
+              <BlockChain
+                position="absolute"
+                top="130px"
+                left="260px"
+                icon={blast}
+              />
+              <BlockChain
+                position="absolute"
+                top="70px"
+                left="310px"
+                icon={Scroll}
+              />
+            </Flex>
+          )}
+
           {/* <Image w="184px" h="128px" src={intIcon3} /> */}
-          <Image
-            w="236px"
-            h="185px"
-            marginLeft="120px"
-            marginTop="-50px"
-            src={intBegger}
-          />
+          {!isShowAnimation && (
+            <Image
+              w="236px"
+              h="185px"
+              marginLeft="120px"
+              marginTop="-50px"
+              src={intBegger}
+            />
+          )}
         </Flex>
       </Flex>
-      <Image marginTop="60px" marginBottom="53px" src={mint} />
+      <Image
+        _hover={buttonHover}
+        onClick={() => {
+          setIsShowAnimation(!isShowAnimation);
+        }}
+        marginTop="100px"
+        marginBottom="53px"
+        src={mint}
+      />
       <Text fontSize="32px" fontWeight="400">
         Rap the pac to mint
       </Text>
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,7 +1,105 @@
-import React, { Component, useEffect } from "react";
+"use client";
 
-function Index() {
-  return <div>1</div>;
-}
+import { Box, chakra } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
-export default Index;
+const Coin = ({ x, y, onCoinCollected }) => {
+  const [isCollected, setIsCollected] = useState(false);
+
+  const handleCoinCollected = () => {
+    setIsCollected(true);
+    onCoinCollected();
+  };
+
+  useEffect(() => {
+    if (isCollected) {
+      return;
+    }
+
+    const interval = setInterval(() => {
+      if (y >= window.innerHeight - 20) {
+        clearInterval(interval);
+        handleCoinCollected();
+      }
+    }, 10);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isCollected, y, handleCoinCollected]);
+
+  return (
+    !isCollected && (
+      <Box
+        w={8}
+        h={8}
+        bg="yellow.400"
+        borderRadius="full"
+        position="absolute"
+        style={{ top: y, left: x }}
+      />
+    )
+  );
+};
+
+const Plate = () => {
+  const [x, setX] = useState(window.innerWidth / 2 - 24);
+
+  const handleMouseMove = (event) => {
+    setX(event.clientX - 24);
+  };
+
+  return (
+    <Box
+      w={48}
+      h={4}
+      bg="gray.300"
+      borderRadius="full"
+      position="absolute"
+      style={{ left: x }}
+      onMouseMove={handleMouseMove}
+    />
+  );
+};
+
+const CoinRain = () => {
+  const [collectedCoins, setCollectedCoins] = useState(0);
+  const [totalCoins] = useState(20);
+
+  const handleCoinCollected = () => {
+    setCollectedCoins((prevCoins) => prevCoins + 1);
+  };
+
+  useEffect(() => {
+    if (collectedCoins === totalCoins) {
+      alert("Congratulations! All coins collected!");
+    }
+  }, [collectedCoins, totalCoins]);
+
+  return (
+    <Box position="relative" h="100vh" overflow="hidden">
+      <Plate />
+
+      {Array(totalCoins)
+        .fill()
+        .map((_, index) => (
+          <Coin
+            key={index}
+            x={Math.random() * (window?.innerWidth - 8)}
+            y={-20 - index * 30}
+            onCoinCollected={handleCoinCollected}
+          />
+        ))}
+    </Box>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <CoinRain />
+    </div>
+  );
+};
+
+export default App;

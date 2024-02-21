9861c2ea3c905f56ffcf699b0ee728bfbbbc725b
Clickcheng110110 <cl15213298081@gmail.com>
2024-02-21 08:38:51 +0800

    feat: update config

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -43,8 +43,8 @@ export interface IConfig {
 const config: Record<string, IConfig> = {
   [blastSepolia.id]: {
     beg: "0x63b21aF3C4a6421954832e389771515080ab1308",
-    routerV2: "0x61BcE66E0236199796bbFf68f7E3fB0ACEDa546D",
-    factory: "0xe37ACCebEf1dC02306701D7F94eeA5D9e149379C",
+    routerV2: "0xb83DC23de672235882540728848B66c2f6EDD3A3",
+    factory: "0x9fdd0684690750aa654F33eBff42174E68FfbbcF",
     weth: "0x4200000000000000000000000000000000000023",
     claim: "0x37095f9440614bCf5a25786bD938D4ace9DCca4e",
   },
